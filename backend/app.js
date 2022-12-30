7871d7cc9633c596f112ae76478af1f6a089d7c1
Meds38 <mohs0038@d.umn.edu>
2022-12-29 20:05:20 -0600

    Adding db connection to app.js

--- backend/app.js
+++ backend/app.js
@@ -9,6 +9,9 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
+const ClientSingleton = require('./client-singleton');
+const db = require('./routes/db');
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
@@ -21,6 +24,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+const client = new ClientSingleton(db.url).client;
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
